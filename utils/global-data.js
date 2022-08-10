24970230e7d39529abf9a5da849477b5b3eca9df
Elfos-Druidas <110882826+Elfos-Druidas@users.noreply.github.com>
2022-08-10 12:05:16 -0300

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,7 +1,7 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Dungeons&Dragons e5';
+    : 'Elfos Druidas de Dungeons&Dragons 5e';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
     : 'Elfos Druidas';
